6fceadfd6d507faa2d61d419b005f8dede88e6b6
codedangerous <cody.robinett@gmail.com>
2025-01-16 22:12:04 +0000

    split the book read into its own function and created function to do a total word count

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 def main():
     path_to_file = "books/frankenstein.txt"
+    book_text = read_book(path_to_file)
+    word_count = count_words(book_text)
+    # print(book_text)
+    print(word_count)
 
-    with open(path_to_file) as f:
+def read_book(file_path):
+    with open(file_path) as f:
         file_contents = f.read()
-    
-    print(file_contents)
+    return file_contents
+
+def count_words(text):
+    words_list = text.split()
+    word_count = len(words_list)
+    return word_count
 
 main()
