ea320f66975fe54554260a0636602864cbb0d0e3
codedangerous <cody.robinett@gmail.com>
2025-01-16 23:09:39 +0000

    add report output

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ def main():
     path_to_file = "books/frankenstein.txt"
     book_text = read_book(path_to_file)
     word_count = count_words(book_text)
-    char_count = count_characters(book_text)
-    # print(book_text)
-    print(char_count)
+    char_counts = count_characters(book_text)
+    char_counts_list = to_list(char_counts)
+    char_counts_list.sort(reverse=True, key=sort_on)
+    print(f"--- Begin report of {path_to_file} ---")
+    print(f"{word_count} words found in the document")
+    print("")
+    for i in range(0, len(char_counts_list)):
+        char = char_counts_list[i]["char"]
+        count = char_counts_list[i]["count"]
+        if char.isalpha():
+            print(f"The '{char}' character was found {count} times")
+    print("--- End Report ---")
 
 def read_book(file_path):
     with open(file_path) as f:
@@ -25,5 +34,15 @@ def count_characters(text):
         else:
             char_dict[char] += 1
     return char_dict
+
+def to_list(dict):
+    new_list = []
+    for item in dict:
+        new_list.append({"char": item, "count": dict[item]})
+    return new_list
+
+def sort_on(dict):
+    return dict["count"]
+
 
 main()
