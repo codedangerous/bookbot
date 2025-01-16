cbeac95691182e07bf3fe3751b8abb0d749fc184
codedangerous <cody.robinett@gmail.com>
2025-01-16 23:21:05 +0000

    clean up the code a little

--- main.py
+++ main.py
@@ -3,16 +3,13 @@ def main():
     book_text = read_book(path_to_file)
     word_count = count_words(book_text)
     char_counts = count_characters(book_text)
-    char_counts_list = to_list(char_counts)
-    char_counts_list.sort(reverse=True, key=sort_on)
+    char_counts_list = to_sorted_list(char_counts)
     print(f"--- Begin report of {path_to_file} ---")
     print(f"{word_count} words found in the document")
     print("")
-    for i in range(0, len(char_counts_list)):
-        char = char_counts_list[i]["char"]
-        count = char_counts_list[i]["count"]
-        if char.isalpha():
-            print(f"The '{char}' character was found {count} times")
+    for item in char_counts_list:
+        if item["char"].isalpha():
+            print(f"The '{item["char"]}' character was found {item["count"]} times")
     print("--- End Report ---")
 
 def read_book(file_path):
@@ -35,10 +32,11 @@ def count_characters(text):
             char_dict[char] += 1
     return char_dict
 
-def to_list(dict):
+def to_sorted_list(dict):
     new_list = []
     for item in dict:
         new_list.append({"char": item, "count": dict[item]})
+    new_list.sort(reverse=True, key=sort_on)
     return new_list
 
 def sort_on(dict):
